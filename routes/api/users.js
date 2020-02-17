770d2e6165a6bfe982608ee7dffbd9d5189dc5c8
unknown <you@example.com>
2020-02-17 09:27:17 -0800

    jwt start

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,88 @@
 const express = require('express');
 const routes = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
-// @routes   GET api/users
-// @desc     Test route
+const User = require('../../models/User');
+
+// @routes   POST api/users
+// @desc     Register user
 // @access   Public
 
-routes.get('/', (req, res, next) => res.send('Users routes'));
+routes.post(
+  '/',
+  [
+    check('name', 'Name is reqiured')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include valid email').isEmail(),
+    check('password', 'Please reqiured password with 6 or more').isLength({
+      min: 6
+    })
+  ],
+  async (req, res, next) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // console.log(req.body)  ;
+
+    const { name, email, password } = req.body;
+    // See if user exits
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'user is a alredy exits' }] });
+      }
+      //Get user gravatar
+      const avater = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+      user = new User({
+        name,
+        email,
+        gravatar,
+        password
+      });
+
+      //Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      //return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      return res.status(500).send('Server error');
+    }
+
+    // res.send('Users Register');
+  }
+);
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const connectDB = require('./config/db');
 const app = express();
 // connect database
 connectDB();
-
+//Init Middleware
+app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('API Running'));
 
 app.use('/api/users', require('./routes/api/users'));
